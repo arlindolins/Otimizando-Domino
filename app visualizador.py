adfd818cb0a1861458be7afd32a0ef560741fc2f
Aflneto <arlindoflins@hotmail.com>
2025-05-18 13:14:00 -0300

    "Alterar nome de arquivo"

---
+++ app visualizador.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask, jsonify, render_template
+from domino_v3 import simular_rodada
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/simular')
+def simular():
+    resultado = simular_rodada()
+    return jsonify(resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True)
